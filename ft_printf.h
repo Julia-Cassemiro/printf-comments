7ca53f80a78bf2526de1db1f2298293667e9f823
Julia-Cassemiro <jg-cassemiro@hotmail.com>
2021-06-29 15:31:15 -0300

    a

---
+++ ft_printf.h
@@ -0,0 +1,34 @@
+#ifndef PRINTF_H
+# define PRINTF_H
+
+# include <unistd.h>
+# include <stdlib.h>
+# include <string.h>
+# include <stdarg.h>
+# include <stdio.h>
+
+//CRIA UMA ESTRUTURA IGUAL FOI CRIADO NA GNL
+// a gente faz isso pq o print f printa em varios formatos, e são as flags que vão nos proporcionar isso
+// essas informações serão usadas pelas flags, mas elas precisam estar em algum lugar (struct)
+// portanto vamo criar as struct q mostra as coisas especifiicas
+
+typedef struct s_print			 
+{		
+	va_list  args;	//conteudo para impressão		  
+	int   wdt;	  // size, tamanho, largura, width, wdt
+	int   prc;	  // precisão	  
+	int   zero;	 // veio zero	 
+	int   pnt;	  // ponto . 
+	int   dash;	 // - traço 
+	int   tl;	// total_length (return value)	  
+	int   sign;	// pos ou neg numero	   
+	int   is_zero;	//  numer zero		   
+	int   porc;	// %	   
+	int   spa;	// space flag ' '	  
+}	va_list;
+
+ft_printf(const char *sms, ...);
+t_print 	*ft_initialise(t_print *arrow);
+int			(va_list *content, const char *sms, int cont;
+
+# endif
---
+++ src/ft_maybe.c
@@ -0,0 +1,99 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_maybe.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jgomes-c <jgomes-c@student.42sp.org.br>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/06/29 15:24:52 by jgomes-c          #+#    #+#             */
+/*   Updated: 2021/06/29 15:30:46 by jgomes-c         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "print.f"
+
+
+int ft_maybe_zero(t_print *content, const char *sms, int cont)
+{
+	int count;
+
+	count = 0;
+	cont++;
+	content->zero = 1;
+	if (ft_isdigit(sms[cont]))
+	{
+		count = ft_atoi(sms[cont]);
+		cont = cont + ft_numlen(count);
+	}
+	content->wdt = count;
+	return (cont);
+}
+
+ft_estrelinha(va_list *content, const char *sms, int cont)
+{
+    cont++;//verifica o proximo
+	//se n tem ponto, o * é o width
+    if (!(content->pnt))  // ou seja, se não tem ponto, n tem precisão (n tem um tamanho certo para impressão)
+		content->wdt = va_arg(content->args, int); //se nao tiver o . ele da pegando o argumento da * e colocando em width
+	else
+	//caso tenha ponto, o * é a precisão
+		content->pnt = va_arg(content->args, int);  // se tem ponto, o * é o argumento da precisão então ele coloca na precisão
+	if (sms[cont] == '.')
+		ft_maybe_precision(content, sms, cont);
+	while (sms[cont] == '*')
+		cont++;
+	return (cont);
+}
+
+ft_maybe_precision(va_list *content, const char *sms, int cont)
+{
+	int count;
+
+	count = 0;
+	cont++;
+	content->pnt = 1;
+	if (ft_isdigit(sms[cont])) //if veio o numero depois
+	{
+		count = ft_atoi(sms[cont]);
+		cont = cont + ft_numlen(count); //o num len retorna a qntd de caracteres certo para impressão
+	}
+	content->prc = count; //retorna a qnd de caracteres necessarios para imprimir
+	if (sms[cont] == '*')
+		cont = ft_maybe_estrelinha(content, sms, cont); //ele ve todas as estrelinhas
+	while (sms[cont] != '*')
+		cont++;
+	return (cont);
+}
+
+ft_maybe_width(va_list *content, const char sms, int cont)
+{
+	int count;
+
+	count = 0;
+	if (ft_isdigit(sms[cont]))
+	{
+		count = ft_atoi(sms[cont]); //passa a string pra um numero inteiro
+		cont = cont + ft_numlen(count); // retorna a qnd de caracteres necessarios para imprimir n
+	}
+	content->width = count;
+	if (sms[cont] == '.')
+		cont = ft_precision(content, sms, cont);
+	return (cont);
+}
+
+ft_maybe_tracinho(t_print *content, const char *sms, int cont)
+{
+	cont++;
+	if(content->dash == 1) //o - pode repetir varias vezes
+	{
+		while (sms[cont] == '-' || sms[cont] == '0')
+			cont++;
+		return (cont);
+	}
+	content->dash = 1;
+	// content->zero = 0;
+	while (sms[cont] == '-' || sms[cont] == '0')
+		cont++;
+	cont = ft_maybe_width(content, sms, cont); //vai la ver se tem width
+	return (cont);
+}
