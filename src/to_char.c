a2f23eee2455b3cfb362401c28563ea355dfcaca
Julia-Cassemiro <jg-cassemiro@hotmail.com>
2021-07-01 16:45:37 -0300

    output char | with comments

--- src/to_char.c
+++ src/to_char.c
@@ -6,7 +6,7 @@
 /*   By: jgomes-c <jgomes-c@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/06/29 21:21:30 by jgomes-c          #+#    #+#             */
-/*   Updated: 2021/07/01 15:52:09 by jgomes-c         ###   ########.fr       */
+/*   Updated: 2021/07/01 16:42:50 by jgomes-c         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,7 +18,7 @@ void	to_char(t_print *content)
 	char	a;
 
 	//estamos passando um argumento
-	a = va_arg(content->args, int); // Cada chamada para va_arg modifica ap para que a próxima chamada retorne o próximo argumento.
+	a = va_arg(content->args, int); // Cada chamada para va_arg modifica ap para que a próxima chamada retorne o próximo argumento
 									// O tipo de parâmetro é um nome de tipo de valor que ap aponta.
 	content->tl = + write(1, &a, 1);
 }
