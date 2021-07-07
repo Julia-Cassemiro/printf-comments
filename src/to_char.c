d24ae9ce85af62b96fa8906e6fafc552e3daf124
Julia-Cassemiro <jg-cassemiro@hotmail.com>
2021-07-07 02:52:52 -0300

    a

--- src/to_char.c
+++ src/to_char.c
@@ -6,7 +6,7 @@
 /*   By: jgomes-c <jgomes-c@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/06/29 21:21:30 by jgomes-c          #+#    #+#             */
-/*   Updated: 2021/07/07 02:39:42 by jgomes-c         ###   ########.fr       */
+/*   Updated: 2021/07/07 02:52:44 by jgomes-c         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,7 +26,7 @@ void	ft_to_char(t_print *content)
 		while (content->wdt-- > 0)
 			content->tl += write(1, " ", 1); //se n tem traço, fica pro lado direito
 	}
-	content->tl = write(1, &c, 1); //mas se tem traço, primeiro escreve
+	content->tl += write(1, &c, 1); //mas se tem traço, primeiro escreve
 	if (content->wdt && content->dash) //n verifica prc, pq c não tem prc
 	{
 		while (content->wdt-- > 0)
