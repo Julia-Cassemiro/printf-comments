a24285b687e448b378ca314b2490384757934f31
Julia-Cassemiro <jg-cassemiro@hotmail.com>
2021-07-07 02:33:11 -0300

    up

--- src/to_char.c
+++ src/to_char.c
@@ -6,7 +6,7 @@
 /*   By: jgomes-c <jgomes-c@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/06/29 21:21:30 by jgomes-c          #+#    #+#             */
-/*   Updated: 2021/07/07 02:25:52 by jgomes-c         ###   ########.fr       */
+/*   Updated: 2021/07/07 02:32:58 by jgomes-c         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,12 +20,29 @@ void	ft_to_char(t_print *content)
 	//estamos passando um argumento
 	c = va_arg(content->args, int); // Cada chamada para va_arg modifica ap para que a próxima chamada retorne o próximo argumento.
 									// O tipo de parâmetro é um nome de tipo de valor que ap aponta.
-	ft_update_lenght(content, 0); //ajusta o wdt e a prc
+	ft_update_lenght(content, 1); //ajusta o wdt e a prc
 	if (content->wdt && !content->dash) //n verifica prc, pq c não tem prc
+	{
 		while (content->wdt-- > 0)
 			content->tl += write(1, " ", 1); //se n tem traço, fica pro lado direito
+	}
 	content->tl = write(1, &c, 1); //mas se tem traço, primeiro escreve
 	if (content->wdt && content->dash) //n verifica prc, pq c não tem prc
+	{
 		while (content->wdt-- > 0)
-			content->tl += write(1, " ", 1);; //se tem traço, fica pro lado esquerdo
+			content->tl += write(1, " ", 1); //se tem traço, fica pro lado esquerdo
+	}
 }
+
+// void	ft_output_char(t_print *content)
+// {
+// 	char	a;
+
+// 	a = va_arg(content->args, int);
+// 	ft_update_lenght(content, 1);
+// 	if (content->wdt && !content->dash)
+// 		ft_right_cs(content, 0);
+// 	content->tl += write(1, &a, 1);
+// 	if (content->wdt && content->dash)
+// 		ft_left_cs(content, 0);
+// }
