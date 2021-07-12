b280a254683e61ab0ae1f40028b317d4528d853d
Julia-Cassemiro <jg-cassemiro@hotmail.com>
2021-07-12 17:00:51 -0300

    ai deus

--- src/ft_lenght.c
+++ src/ft_lenght.c
@@ -6,7 +6,7 @@
 /*   By: jgomes-c <jgomes-c@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/06/29 18:32:31 by jgomes-c          #+#    #+#             */
-/*   Updated: 2021/07/12 16:51:57 by jgomes-c         ###   ########.fr       */
+/*   Updated: 2021/07/12 16:59:54 by jgomes-c         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -55,10 +55,10 @@ void	ft_change_zero(t_print *content)
 	{
 		if (!content->prc)
 		{
-			while (content->zero && content->wdt-- >= 0) 
-				content->tl += write(1, "0", 1);
 			while (!content->zero && content->wdt-- > 0)
 				content->tl += write(1, " ", 1);
+			while (content->zero || content->wdt-- > 0) 
+				content->tl += write(1, "0", 1);
 		}
 		else
 		{
