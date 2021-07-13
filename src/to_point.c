29e20a0241e7a5ce66718d71e57cb36fe27f8fca
Julia-Cassemiro <jg-cassemiro@hotmail.com>
2021-07-12 23:22:44 -0300

    eh

--- src/to_point.c
+++ src/to_point.c
@@ -6,7 +6,7 @@
 /*   By: jgomes-c <jgomes-c@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/07/12 15:19:31 by jgomes-c          #+#    #+#             */
-/*   Updated: 2021/07/12 16:21:20 by jgomes-c         ###   ########.fr       */
+/*   Updated: 2021/07/12 23:21:16 by jgomes-c         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,24 +20,25 @@ void ft_to_pointer(t_print *content)
 
 	len = 2;
 	p = (unsigned long)va_arg(content->args, void *);
-	if (!p && content->pnt)
+	if (!p && content->pnt && content->prc > 0)
 	{
 		ft_nullp(content);
 		return ;
 	}
 	else
 		len += ft_numlen_base(p, 16);
-	if (content->wdt > content->prc)
-		ft_update_lenght(content, len);
-	content->prc = content->prc + 2 - len;
-	ft_right(content);
-	content->tl += write(1, "0x", 2);
-	if (content->prc)
+	if (!content->prc || content->wdt > content->prc)
 	{
-		while (content->prc -- > 0)
-			content->tl += write(1, "0", 1);
+		ft_update_lenght(content, len);
+		ft_right(content);
 	}
-	content->tl += ft_putnbr_base(p, "0123456789abcdef");
+	else
+		content->prc = content->prc + 2 - len;
+	content->tl += write(1, "0x", 2);
+	while (content->prc -- > 0)
+		content->tl += write(1, "0", 1);
+	if ((p && content->pnt))
+		content->tl += ft_putnbr_base(p, "0123456789abcdef");
 	ft_left(content); 	
 }
 
