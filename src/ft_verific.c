0258d0c1b2e6c1ad91adcada9348fee1b51835a8
Julia-Cassemiro <jg-cassemiro@hotmail.com>
2021-07-14 15:12:23 -0300

    a

--- src/ft_verific.c
+++ src/ft_verific.c
@@ -6,7 +6,7 @@
 /*   By: jgomes-c <jgomes-c@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/06/29 13:40:38 by jgomes-c          #+#    #+#             */
-/*   Updated: 2021/07/14 14:37:18 by jgomes-c         ###   ########.fr       */
+/*   Updated: 2021/07/14 15:12:06 by jgomes-c         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,15 +29,13 @@ int 	ft_verific_letter(t_print *content, const char *sms, int cont)
 		ft_to_percent(content);
 	else if (sms[cont] == 'p')
 		ft_to_pointer(content);
-	else
-		content->tl += write(1, "", 1);
 	return (cont);
 }
 
 // while n é o cspdiuxX%
 int		ft_verific_symb(t_print *content, const char *sms, int cont)// aonde vamos cmc o check, poisção
 {
-	while (!(ft_isalpha(sms[cont]) || (sms[cont] == '%') || (sms[cont] == '\0')))
+	while (!(ft_isalpha(sms[cont]) || (sms[cont] == '%')))
 	{
 		if (sms[cont] == '-')
 			cont = ft_maybe_tracinho(content, sms, cont);
