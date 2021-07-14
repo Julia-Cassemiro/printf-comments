a18bfe27e2454355a95421059c65a4b778770977
Julia-Cassemiro <jg-cassemiro@hotmail.com>
2021-07-14 14:38:42 -0300

    a

--- src/ft_verific.c
+++ src/ft_verific.c
@@ -6,7 +6,7 @@
 /*   By: jgomes-c <jgomes-c@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/06/29 13:40:38 by jgomes-c          #+#    #+#             */
-/*   Updated: 2021/07/14 14:25:49 by jgomes-c         ###   ########.fr       */
+/*   Updated: 2021/07/14 14:37:18 by jgomes-c         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -49,7 +49,6 @@ int		ft_verific_symb(t_print *content, const char *sms, int cont)// aonde vamos 
 			cont = ft_maybe_estrelinha(content, sms, cont);
 		else if (ft_isdigit(sms[cont])) //if tem um numero la, define o width
 			cont = ft_maybe_width(content, sms, cont);
-		
 	}
 	ft_verific_letter(content, sms, cont);
 	ft_reset(content);
