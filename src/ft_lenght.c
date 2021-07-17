4658abd2e573ec999d36788e1641c0aabb86f9d9
Julia-Cassemiro <jg-cassemiro@hotmail.com>
2021-07-17 03:27:48 -0300

    a

--- src/ft_lenght.c
+++ src/ft_lenght.c
@@ -6,7 +6,7 @@
 /*   By: jgomes-c <jgomes-c@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/06/29 18:32:31 by jgomes-c          #+#    #+#             */
-/*   Updated: 2021/07/17 02:00:50 by jgomes-c         ###   ########.fr       */
+/*   Updated: 2021/07/17 03:26:17 by jgomes-c         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -83,11 +83,15 @@ void	ft_change_zero(t_print *content)
 			}
 			else
 				content->wdt -= content->prc;
+			if (content->zero && content->plus)
+				content->tl += write(1, "+", 1);
 			while (content->dash && content->prc-- > 0)
+				content->tl += write(1, "0", 1);
+			while (content->wdt-- > 0 && content->zero && content->plus)
 				content->tl += write(1, "0", 1);
 			while (content->wdt-- > 0)
 				content->tl += write(1, " ", 1);
-			if (content->plus)
+			if (!content->zero && content->plus)
 				content->tl += write(1, "+", 1);
 			while (!content->dash && content->prc-- > 0)
 				content->tl += write(1, "0", 1);
