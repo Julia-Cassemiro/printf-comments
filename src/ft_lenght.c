0046332265d993298e0e7fc4c84da222c59f0bd3
Julia-Cassemiro <jg-cassemiro@hotmail.com>
2021-07-16 22:59:10 -0300

    a

--- src/ft_lenght.c
+++ src/ft_lenght.c
@@ -6,7 +6,7 @@
 /*   By: jgomes-c <jgomes-c@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/06/29 18:32:31 by jgomes-c          #+#    #+#             */
-/*   Updated: 2021/07/16 22:23:20 by jgomes-c         ###   ########.fr       */
+/*   Updated: 2021/07/16 22:58:43 by jgomes-c         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,14 +15,7 @@
 #include "../libft/libft.h"
 
 void	ft_update_lenght(t_print *content, int len)
-{
-	if (content->plus)
-	{
-		if (content->sign)
-			content->plus = 0;
-		else
-			content->tl += write(1, "+", 1);
-	}
+{	
 	if (content->sign) //if o é negativo
 	{
 		if (content->wdt) //wdt fica cm -1, pq um dos espaços é pro -
@@ -89,6 +82,13 @@ void	ft_right(t_print *content)
 {
 	if (content->spa)
 		content->tl += write(1, " ", 1);
+	if (content->plus)
+	{
+		if (content->sign)
+			content->plus = 0;
+		else
+			content->tl += write(1, "+", 1);
+	}
 	if (content->sign && (content->zero || content->dash || (!content->wdt && !content->prc)))
 	{
 		content->tl += write(1, "-", 1); //ajuste para por o sinal antes, lembrando q n tem wdt
