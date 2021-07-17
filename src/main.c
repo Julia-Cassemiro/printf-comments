77e69952af37aab594e0c034d7e677922d46ced0
Julia-Cassemiro <jg-cassemiro@hotmail.com>
2021-07-16 21:19:32 -0300

    main

--- src/main.c
+++ src/main.c
@@ -4,35 +4,11 @@
 
 int main()
 {
-    int		a = 2;
-	int		b = 0;
-	char	c = 'a';
-	int		d = 2147483647;
-	int		e = -2147483648;
-	int		f = 42;
-	int		g = 25;
-	int		h = 4200;
-	int		i = 8;
-	int		j = -12;
-	int		k = 123456789;
-	int		l = 0;
-	int		m = -12345678;
-	char	*n = "abcdefghijklmnop";
-	char	*o = "-a";
-	char	*p = "-12";
-	char	*q = "0";
-	char	*r = "%%";
-	char	*s = "-2147483648";
-	char	*t = "0x12345678";
-	char	*u = "-0";
-
-    static char *s_hidden = "hi low\0don't print me lol\0";
-
-    printf(" seu retorno %d", printf("%01.d", 0));
+    printf(" seu retorno %d", printf(" % 1s", ""));
 	printf("\n");
     printf("\n");
     printf("\n");
-    printf(" meu retorno %d", ft_printf("%01.d", 0));
+    printf(" meu retorno %d", ft_printf(" % 1s", ""));
     //ft_printf("teste |%5c|\n", 'a');
     return 0;
 }
