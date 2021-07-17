823b51b3458d7e777e52961dc905376cfde21161
Julia-Cassemiro <jg-cassemiro@hotmail.com>
2021-07-17 15:43:01 -0300

    zero ++

--- src/ft_zero.c
+++ src/ft_zero.c
@@ -6,12 +6,58 @@
 /*   By: jgomes-c <jgomes-c@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/07/17 12:43:40 by jgomes-c          #+#    #+#             */
-/*   Updated: 2021/07/17 13:45:53 by jgomes-c         ###   ########.fr       */
+/*   Updated: 2021/07/17 15:42:16 by jgomes-c         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/ft_printf.h"
 #include "../libft/libft.h"
+
+// void ft_plus(t_print *content)
+// {
+// 	if (content->spa)
+// 	{
+// 		content->tl += write(1, " ", 1);
+// 		content->wdt -= content->spa;
+// 	}
+// 	content->wdt -= content->plus;
+// 	content->prc -= content->plus;
+// 	if (!content->prc)
+// 	{
+// 		if (content->plus)
+// 			content->tl += write(1, "+", 1);
+// 		while (content->zero && content->wdt-- > 0) 
+// 			content->tl += write(1, "0", 1);
+// 		while (!content->zero && content->wdt-- > 0)
+// 			content->tl += write(1, " ", 1);
+// 	}
+// 	else
+// 	{
+// 		if (content->dash || (content->zero && !content->pnt))
+// 		{
+// 			content->tl += write(1, "+", 1);
+// 			content->plus = 0;
+// 		}
+// 		content->wdt -= content->prc + 1;
+// 		content->prc += 1;
+// 	}
+// 	while (content->dash && content->prc-- > 0)
+// 		content->tl += write(1, "0", 1);
+// 	if (!content->prc && content->zero)
+// 	{
+// 		while (content->wdt-- > 0 )
+// 			content->tl += write(1, "0", 1);
+// 	}
+// 	while (content->wdt-- > 0)
+// 		content->tl += write(1, " ", 1);
+// 	if (!content->dash && content->plus)
+// 		content->tl += write(1, "+", 1);
+// 	while (!content->dash && content->prc-- > 0)
+// 		content->tl += write(1, "0", 1);
+// 	if (content->prc && content->dash && !content->wdt)//caso tenha prc e dash, mas n tem width
+// 		while (!content->wdt && content->prc-- > 0)
+// 			content->tl += write(1, "0", 1);	
+// }
 
 void ft_plus(t_print *content)
 {
@@ -25,7 +71,10 @@ void ft_plus(t_print *content)
 	if (!content->prc)
 	{
 		if (content->plus)
+		{
 			content->tl += write(1, "+", 1);
+			content->plus = 0;
+		}
 		while (content->zero && content->wdt-- > 0) 
 			content->tl += write(1, "0", 1);
 		while (!content->zero && content->wdt-- > 0)
@@ -41,7 +90,7 @@ void ft_plus(t_print *content)
 		content->wdt -= content->prc + 1;
 		content->prc += 1;
 	}
-	while (content->dash && content->prc-- > 0)
+	while (content->dash || content->prc-- > -1)
 		content->tl += write(1, "0", 1);
 	if (!content->prc && content->zero)
 	{
